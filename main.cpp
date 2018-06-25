b8c6e0904bc698e59a0a10fe8ce0cdab8f6d1980
MerceRui <38316992+MerceRui@users.noreply.github.com>
2018-06-25 16:23:22 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,9 @@ using namespace std;
 #include"BigNum.h"
 int main()
  {
- 	BigNum a,b;
+ 	BigNum a,b,c;
  	cin>>a>>b;
- 	cout<<a<<b;
+  c=a;
+ 	cout<<a<<endl<<c<<endl<<a+b<<endl<<a-b<<endl<<a*b<<endl;
  	return 0;
 }
