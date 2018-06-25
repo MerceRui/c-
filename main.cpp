dc4325834e59af73e64112e25d02977a91929649
MerceRui <38316992+MerceRui@users.noreply.github.com>
2018-06-25 16:16:59 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
-using namespace std;
 #include <string>
-#include <vector>
+using namespace std;
+#include"BigNum.h"
 int main()
  {
- 	int b[5],i;
- 	string a;
- 	for(i=0;i<5;i++)
-	cin>>b[i];
-    for(i=0;i<5;i++)
-		a+=(char)(b[i]+'0');
- 	cout<<a;
-	return 0;
+ 	BigNum a,b;
+ 	cin>>a>>b;
+ 	cout<<a<<b;
+ 	return 0;
 }
